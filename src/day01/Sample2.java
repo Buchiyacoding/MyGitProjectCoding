1a29f70ddfa60f6618d42c45bef6ace30b814b36
Admin <buchiya95@gmail.com>
2023-03-12 10:30:16 +0530

    this is my first commit msg

--- src/day01/Sample2.java
+++ src/day01/Sample2.java
@@ -3,7 +3,7 @@ package day01;
 public class Sample2 {
 
 	public static void main(String[] args) {
-		System.out.println("This is my first sample program");
+		System.out.println("This is my second sample program");
 
 	}
 
